285f1694c21aa96b23338ed2acedbe6ceebddde4
dariaKretiuk <kretyukdasha@gmail.com>
2022-02-14 17:15:19 +0700

    Initial Commit

---
+++ iOS5-HW10-KretiukDasha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS5-HW10-KretiukDasha
+//
+//  Created by Дарья Кретюк on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
